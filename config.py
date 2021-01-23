4a35197cb5ea37a818ba8e30e249b61c27f2e1a0
jakestrj <jakestrj@berkeley.edu>
2021-01-22 17:28:28 -0800

    Config

---
+++ config.py
@@ -0,0 +1,18 @@
+config = {
+    "THREADS": 4,
+    "DATADIR" : "data",
+    "TRAIN_VID" : "train.mp4",
+    "TRAIN_Y" : "train.txt",
+    "TEST_VID" : "test.mp4",
+    "REPROCESS" : False,
+    "TRAIN": False,
+
+    "batch_size" : 8,
+    "test_batch_size" : 8,
+    "epochs" : 40,             
+    "lr" : 1e-4,             
+    "split" : 0.2,
+    "delta" : 1,
+    "depth" : 20,
+    "randomize" : True,
+}
